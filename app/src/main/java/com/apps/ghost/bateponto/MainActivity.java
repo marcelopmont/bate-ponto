24029b8898379a9619d3c7421d9bb414684c0e16
Marcelo <marcelo.montanher14@gmail.com>
2017-08-01 21:25:45 -0300

    set main ids

--- app/src/main/java/com/apps/ghost/bateponto/MainActivity.java
+++ app/src/main/java/com/apps/ghost/bateponto/MainActivity.java
@@ -2,12 +2,42 @@ package com.apps.ghost.bateponto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.home_balance)
+    TextView balance;
+    @BindView(R.id.home_clock_in)
+    Button clockIn;
+    @BindView(R.id.home_start_time)
+    TextView startTime;
+    @BindView(R.id.home_end_time)
+    TextView endTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setupView();
+    }
+
+    private void setupView() {
+
+        clockIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
